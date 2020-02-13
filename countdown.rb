7334dfca33750722b9728b4507d69df16d20e76b
Todd Norton <toddmnorton@live.com>
2020-02-13 20:49:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,19 @@
 #write your code here
-number = 10 
+seconds_to_midnight = 30
 
-def countdown
-  while #{number} > 0 
-    puts "#{number SECOND(S)!"
-    number -= 1 
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
